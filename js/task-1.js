c7708e531edb54c5bb845f9ac80b501ab5fe9a3e
Miami <alex.miami.petrov@gmail.com>
2024-06-13 12:36:26 +0300

    m

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const categoriesList = document.querySelectorAll("#categories .item");
-
-console.log(`Number of categories: ${categoriesList.length}`);
-
-categoriesList.forEach((category) => {
-  const categoryName = category.querySelector("h2").textContent;
-  const itemCount = category.querySelectorAll("li").length;
-  console.log(`Category: ${categoryName} (Elements: ${itemCount})`);
-});
